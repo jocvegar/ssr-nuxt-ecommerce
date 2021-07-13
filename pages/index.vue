f6f412d89045804dbabbd8792752d07f3a8c7c43
Jose Vega <jocvegar@gmail.com>
2021-07-13 09:18:02 -0600

    styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,6 @@
 <template>
   <div>
-    <Tutorial />
-
-    <div>
-      <b-card
-        :title="merchant.business_name"
-        img-top
-        tag="article"
-        style="max-width: 20rem;"
-        class="mb-2"
-      >
-        <b-card-text>
-          merchant: {{ merchant.business_name }}
-          <h3><NuxtLink to="/categories">Categories NuxtLink</NuxtLink></h3>
-          <category-list :categories="categories"></category-list>
-          <hr />
-          <h3><nuxt-link to="/products">Products nuxt-link </nuxt-link></h3>
-          <product-list :products="products"></product-list>
-        </b-card-text>
-
-        <b-button href="#" variant="primary">Go somewhere</b-button>
-      </b-card>
-    </div>
+    <Tutorial :products="products" :categories="categories" />
   </div>
 </template>
 
